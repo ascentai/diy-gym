fixing ignored rotation in respawn (#28)

diy_gym/addons/misc/respawn.py
@@ -8,16 +8,25 @@ class Respawn(Addon):
         super(Respawn, self).__init__(parent, config)
 
         self.uid = parent.uid
-        self.origin = p.getBasePositionAndOrientation(self.uid)[0]
+        self.initial_pose = p.getBasePositionAndOrientation(self.uid)
         self.position_range = config.get('position_range', [0., 0., 0.])
         self.rotation_range = config.get('rotation_range', [0., 0., 0.])
-        self.joint_range = config.get('joint_range', [0., 0., 0.])
 
         self.once = config.get('once', False)
 
         self.pose = self.generate_pose()
 
         self.reset()
+
+    def quaternion_multiply(self, quaternion1, quaternion0):
+        ''' Return multiplication of two quaternions. '''
+        x0, y0, z0, w0 = quaternion0
+        x1, y1, z1, w1 = quaternion1
+        return np.array((
+            x1*w0 + y1*z0 - z1*y0 + w1*x0,
+            -x1*z0 + y1*w0 + z1*x0 + w1*y0,
+            x1*y0 - y1*x0 + z1*w0 + w1*z0,
+            -x1*x0 - y1*y0 - z1*z0 + w1*w0), dtype=np.float64)
 
     def reset(self):
         if not self.once:
@@ -26,5 +35,5 @@ class Respawn(Addon):
         p.resetBasePositionAndOrientation(self.uid, *self.pose)
 
     def generate_pose(self):
-        return ((np.random.random(3) - 0.5) * self.position_range + self.origin,
-                p.getQuaternionFromEuler((np.random.random(3) - 0.5) * self.rotation_range))
+        return ((np.random.random(3) - 0.5) * self.position_range + self.initial_pose[0],
+                self.quaternion_multiply(self.initial_pose[1], p.getQuaternionFromEuler((np.random.random(3) - 0.5) * self.rotation_range)))